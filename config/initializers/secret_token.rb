8ea76e63a8c6a5d54899e7baa3f57191b74658a2
gps13 <g.psistakis@gmail.com>
2013-09-27 00:08:23 +0300

    Added all the things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Favio::Application.config.secret_token = 'b136ee765f53488062e72f52e136ee2e341398bf24c606d75cc6260236ed557c4d804a70001a0a7cd00554bc2307a1c0d5ef64c26e8ed6194d8123361d8f9292'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Favio::Application.config.session_store :cookie_store, key: '_favio_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Favio::Application.config.session_store :active_record_store
