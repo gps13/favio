2a5c6557cdc0eae362f61ba8a7fac905437e41b1
gps13 <g.psistakis@gmail.com>
2013-10-01 15:05:33 +0300

    installed dejan/auto_html

--- app/models/favinio.rb
+++ app/models/favinio.rb
@@ -1,4 +1,11 @@
 class Favinio < ActiveRecord::Base
   attr_accessible :description, :title
   has_many :votes, dependent: :destroy
+  auto_html_for :description do
+
+    image
+    youtube(:width => 400, :height => 250)
+    link :target => "_blank", :rel => "nofollow"
+    simple_format
+  end
 end
